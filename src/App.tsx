98d3f497e46631de6b8f1509e65d0ce7df572408
singuerinc <nahuel.scotti@gmail.com>
2022-04-06 15:18:20 +0200

    feat: with real date

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import { IconMaximize } from "@tabler/icons";
 import { useMachine, useSelector } from "@xstate/react";
-import { addMilliseconds, addMinutes, format } from "date-fns";
+import { addMilliseconds, addMinutes, differenceInMilliseconds, format } from "date-fns";
 import * as React from "react";
 import { useCallback } from "react";
-import { assign, createMachine } from "xstate";
+import { assign, createMachine, State } from "xstate";
 
 const MIN_0 = 0;
 const MIN_5 = 5 * 60 * 1000;
 const MIN_25 = 25 * 60 * 1000;
 
-export const timerMachine = createMachine(
+type Context = {
+  totalTime: number;
+  startAt: Date;
+  endAt: Date;
+  accumulated: Date;
+};
+
+export const timerMachine = createMachine<Context>(
   {
     initial: "idle",
     context: {
       totalTime: MIN_0,
+      startAt: new Date(),
+      endAt: new Date(),
       accumulated: new Date(),
     },
     on: {
@@ -26,18 +34,18 @@ export const timerMachine = createMachine(
     },
     states: {
       idle: {
-        entry: ["reset"],
+        entry: ["zero"],
         on: {
           FORWARD: [
             {
-              cond: (ctx: any) => ctx.totalTime > 0,
+              cond: (ctx: Context) => ctx.totalTime > 0,
               target: "running",
             },
           ],
         },
       },
       running: {
-        entry: ["update"],
+        entry: ["reset", "update"],
         invoke: {
           src: "tick",
         },
@@ -45,7 +53,7 @@ export const timerMachine = createMachine(
           FORWARD: "idle",
           TICK: [
             {
-              cond: (ctx: any) => ctx.accumulated > 0,
+              cond: (ctx: Context) => ctx.accumulated.getTime() > 0,
               actions: ["update"],
             },
             {
@@ -60,18 +68,35 @@ export const timerMachine = createMachine(
     actions: {
       add5: assign({
         totalTime: (ctx) => ctx.totalTime + MIN_5,
-        accumulated: (ctx) => addMinutes(ctx.accumulated, 5),
+        accumulated: (ctx) => addMilliseconds(ctx.accumulated, MIN_5),
+        endAt: (ctx) => addMilliseconds(ctx.endAt, MIN_5),
       }),
       add25: assign({
         totalTime: (ctx) => ctx.totalTime + MIN_25,
-        accumulated: (ctx) => addMinutes(ctx.accumulated, 25),
+        accumulated: (ctx) => addMilliseconds(ctx.accumulated, MIN_25),
+        endAt: (ctx) => addMilliseconds(ctx.endAt, MIN_25),
+      }),
+      zero: assign({
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        startAt: (_) => new Date(0),
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        endAt: (_) => new Date(0),
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        accumulated: (_) => new Date(0),
       }),
       reset: assign({
-        accumulated: (_) => new Date(MIN_0),
-        totalTime: (_) => MIN_0,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        startAt: (_) => new Date(),
+        endAt: (ctx) => addMilliseconds(new Date(), ctx.totalTime),
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        // accumulated: (_) => new Date(),
       }),
       update: assign({
-        accumulated: (ctx) => addMilliseconds(ctx.accumulated, -500),
+        accumulated: (ctx) => {
+          const now = new Date();
+          const diff = differenceInMilliseconds(ctx.endAt, now);
+          return addMilliseconds(new Date(0), diff);
+        },
       }),
     },
     services: {
@@ -88,20 +113,14 @@ export const timerMachine = createMachine(
 
 function App() {
   const [, send, service] = useMachine(timerMachine);
-  const accumulated = useSelector(
-    service,
-    (state) => state.context.accumulated
-  );
+  const accumulated = useSelector(service, (state: State<Context>) => state.context.accumulated);
   const forward = useCallback(() => send("FORWARD"), [send]);
   const add5 = useCallback(() => send("ADD_5"), [send]);
 
   return (
     <div className="flex w-full select-none flex-col items-center justify-center">
-      <div
-        className="flex text-8xl font-bold tabular-nums sm:text-[12rem]"
-        onClick={forward}
-      >
-        {format(accumulated, "mm:ss")}
+      <div className="flex flex-col tabular-nums" onClick={forward}>
+        <div className="text-8xl font-bold sm:text-[12rem]">{format(accumulated, "mm:ss")}</div>
       </div>
       <div className="absolute bottom-0 left-0 flex text-2xl opacity-50">
         <button className="p-6 font-light" onClick={add5}>
